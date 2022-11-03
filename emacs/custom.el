024a66829838c7cf643ca206583bc58a3855466a
Erina <75091994+Erinacchu@users.noreply.github.com>
2022-11-03 10:44:41 +0100

    Add files via upload

---
+++ emacs/custom.el
@@ -0,0 +1,18 @@
+
+(setq doom-font (font-spec :family "Noto Sans Mono" :size 16)
+      doom-variable-pitch-font (font-spec :family "Noto Sans Mono" :size 16)
+      )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" "6c531d6c3dbc344045af7829a3a20a09929e6c41d7a7278963f7d3215139f6a7" default))
+ '(package-selected-packages '(elcord)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
